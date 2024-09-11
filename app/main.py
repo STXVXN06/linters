fdc1007a936f80d1200068c50dde6ac3b149df23
Santiago Parra <santiago.parra.0276@eam.edu.co>
2024-09-10 20:53:06 -0500

    Primer commit

---
+++ app/main.py
@@ -0,0 +1,15 @@
+"""Metodo que suma dos numeros"""
+
+
+def suma(num1, num2):
+    """
+    Calcular la suma de dos numeros.
+
+    Args:
+        num1 (int o float): The firs number
+        num2 (int o float): The second number
+
+    Returns:
+        int o float: The sum of num1 and num2.
+    """
+    return num1 + num2
